362ea68b3a02d9ae373a4c64abcba3098c6c6906
Ar4ness <ar4ness@mail.ru>
2017-04-01 09:27:46 +0300

    some changes made

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+$(document).ready(function() {
+  $('.header-slider').slick({
+    accessibility: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    mobileFirst: true
+  });
+});
