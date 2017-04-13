80a9d4aadb0d7e03a62328881c763fbbd942e41c
Ar4ness <ar4ness@mail.ru>
2017-04-13 16:29:04 +0300

    one more problem left. check it

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,32 +11,6 @@ $(document).ready(function() {
     mobileFirst: true
   });
 
-  $('.donors-slider').slick({
-    //accessibility: false,
-    dots: true,
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    speed: 2000,
-    autoplay: true,
-    autoplaySpeed: 3000
-    //cssEase: 'linear',
-    /*responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          infinite: true,
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-    ]*/
-  });
+
+
 });
